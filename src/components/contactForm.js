151e0282d30811391dc3dd2751a505065e9f82cb
Valeriya Druchinina <vchumack@glmedia.org>
2023-02-01 02:31:24 +0200

    added form section and footer

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,41 +1,183 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { HiOutlineHeart } from "react-icons/hi";
+import { Oval } from "react-loader-spinner";
 import s from "@/styles/ContactForm.module.scss";
 
 const ContactForm = () => {
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const onSubmit = async (data) => {
+    setIsLoading(true);
+
+    try {
+      console.log(data);
+      await axios.post(
+        "https://sheet.best/api/sheets/8e3a48aa-d794-45df-b76c-96f5c9cb843e",
+        data
+      );
+      await axios.post(
+        "https://api.telegram.org/bot5826218439:AAFcxuVEYJ9gOaOjEYy7qLre1vkGDseB15o/sendMessage",
+        {
+          chat_id: "-1001762357803",
+          parse_mode: "html",
+          text: JSON.stringify(data),
+        }
+      );
+
+      setIsSubmit(true);
+      reset();
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <section className={s.section}>
+        <div className="wrapper">
+          <div className={s.loader}>
+            <Oval
+              height={80}
+              width={80}
+              color="#00B6B0"
+              secondaryColor="#BEA698"
+            />
+          </div>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className={s.section}>
       <div className="wrapper">
         <div className={s.wrapper}>
-          <h2>Отримати консультацію та пропозицію за вашим запитом</h2>
-          <form>
-            <div className={s.userBox}>
-              <input type="text" name="name" id="name" required />
-              <label for="name">Ваше ім'я</label>
-            </div>
-            <div className={s.userBox}>
-              <input type="text" name="email" id="email" required />
-              <label for="email">Ваша пошта</label>
-            </div>
-            <div className={s.userBox}>
-              <input
-                type="text"
-                name="tel"
-                id="tel"
-                // pattern="/^(\+?\d+)?\s*(\(\d+\))?[\s-]*([\d-]*)$/"
-                required
-              />
-              <label for="tel">Ваш номер телефону</label>
-            </div>
+          {!isSubmit ? (
+            <>
+              <h2 className={s.title}>
+                Отримати консультацію та пропозицію за вашим запитом
+              </h2>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className={s.userBox}>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder=" "
+                    {...register("name", {
+                      required: "Поле обов'язкове для заповнення",
+                      minLength: {
+                        value: 3,
+                        message: "Введіть мінімум 3 символи",
+                      },
+                    })}
+                  />
+                  <label htmlFor="name" className={s.label}>
+                    Ваше ім'я
+                  </label>
+                  <div className={s.error}>
+                    {errors?.name && (
+                      <p>
+                        {errors?.name?.message ||
+                          "Щось раптом пішло не так, повторіть спробу"}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className={s.userBox}>
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    placeholder=" "
+                    {...register("email", {
+                      required: "Поле обов'язкове для заповнення",
+                      pattern: {
+                        value:
+                          /^(?=.{10,63}$)(([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/,
+                        message: "Введіть існуючу адресу пошти",
+                      },
+                    })}
+                  />
+                  <label htmlFor="email" className={s.label}>
+                    Ваша пошта
+                  </label>
+                  <div className={s.error}>
+                    {errors?.email && (
+                      <p>
+                        {errors?.email?.message ||
+                          "Щось раптом пішло не так, повторіть спробу"}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className={s.userBox}>
+                  <input
+                    type="text"
+                    name="tel"
+                    id="tel"
+                    placeholder=" "
+                    {...register("tel", {
+                      required: "Поле обов'язкове для заповнення",
+                    })}
+                  />
+                  <label htmlFor="tel" className={s.label}>
+                    Ваш номер телефону
+                  </label>
+                  <div className={s.error}>
+                    {errors?.tel && (
+                      <p>
+                        {errors?.tel?.message ||
+                          "Щось раптом пішло не так, повторіть спробу"}
+                      </p>
+                    )}
+                  </div>
+                </div>
 
-            <div className={s.btnForm}>
-              <button id="submit" type="submit" className={s.submit}>
-                Відправити
-              </button>
-              <div className={s.confident}>
-                Відправляючи дані, ви погоджуєтеся з{" "}
-                <a href="#">політикою конфіденційності</a>
-              </div>
-            </div>
-          </form>
+                <div className={s.btnForm}>
+                  <button
+                    id="submit"
+                    type="submit"
+                    disabled={!isValid}
+                    className={s.submit}
+                  >
+                    Відправити
+                  </button>
+                  <div className={s.confident}>
+                    Відправляючи дані, ви погоджуєтеся з{" "}
+                    <a href="/privacyPolicy" target="_blanc">
+                      політикою конфіденційності
+                    </a>
+                  </div>
+                </div>
+              </form>
+            </>
+          ) : (
+            <>
+              <p className={s.successSubmit}>
+                Дякуємо за заповнення форми, вашу заявку успішно прийнято. Ми
+                зв'яжемося з вами найближчим часом!
+              </p>
+              <HiOutlineHeart size={50} className={s.icons} />
+              <p className={s.successSubmit}>
+                Повернутися на <a href="/">головну</a>
+              </p>
+            </>
+          )}
         </div>
       </div>
     </section>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,60 @@
+import Link from "next/link";
+import { BsInstagram, BsTelegram, BsLinkedin } from "react-icons/bs";
+
+import s from "@/styles/Footer.module.scss";
 const Footer = () => {
   return (
-    <footer>
-      <div className="wrapper">
-        <div>Розроблено MY Developments 2013</div>
+    <footer className={s.footer}>
+      <div className={`${s.wrapper} wrapper`}>
+        <div className={s.row}>
+          <div className={s.col}>
+            <h3>Наші контакти</h3>
+            <a
+              className={`${s.contactLink} ${s.tel}`}
+              href="tel:+380 93 560 44 06"
+            >
+              +380 93 560 44 06
+            </a>
+            <a
+              className={`${s.contactLink} ${s.tel}`}
+              href="tel:+380 93 560 44 06"
+            >
+              +380 93 560 44 06
+            </a>
+          </div>
+          <div className={s.col}>
+            <h3>Наші соціальні мережі</h3>
+            <div className={s.iconBox}>
+              <Link
+                className={s.icon}
+                href="https://t.me/givishenka"
+                target="_blank"
+              >
+                <BsTelegram />
+              </Link>
+              <Link
+                className={s.icon}
+                href="https://www.instagram.com/gl.media/"
+                target="_blank"
+              >
+                <BsInstagram />
+              </Link>
+              <Link
+                className={s.icon}
+                href="https://www.instagram.com/gl.media/"
+                target="_blank"
+              >
+                <BsLinkedin />
+              </Link>
+            </div>
+          </div>
+          <div className={s.col}>
+            <a className={s.contactLink} href="mailto:group@glmedia.org">
+              group@glmedia.org
+            </a>
+          </div>
+        </div>
+        <p className={s.copywright}>Copyright © 2023. Розроблено MYD</p>
       </div>
     </footer>
   );
