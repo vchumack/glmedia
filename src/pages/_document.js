82f1d47302a74e0b1e676aae02677d9bdc0e4360
Valeriya Druchinina <vchumack@glmedia.org>
2023-02-26 01:58:35 +0200

    del google script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,16 +4,6 @@ export default function Document() {
   return (
     <Html lang="uk">
       <Head />
-      {/* <!-- Google tag (gtag.js) --> */}
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-C73728S2WE"
-      ></script>
-      <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-C73728S2WE');
-      </script>
       <body id="main">
         <Main />
         <NextScript />
